d721b3fb0e1872b039e863da72c826b5ca80a11d
Karim <k.robai@yahoo.ca>
2025-12-26 10:19:34 -0500

    Delete FlexiBase directory

--- FlexiBase/Models/IntegerFieldConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlexiBase.Models
-{
-    public class IntegerFieldConfiguration
-    {
-        public int? MinValue { get; set; }
-        public int? MaxValue { get; set; }
-        public int Step { get; set; } = 1;
-    }
-}
--- FlexiBase/Models/SelectFieldConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlexiBase.Models
-{
-    public class SelectFieldConfiguration
-    {
-        public List<SelectOption> Options { get; set; } = new();
-        public bool AllowCustom { get; set; } = false;
-        public string? DefaultValue { get; set; }
-    }
-}
